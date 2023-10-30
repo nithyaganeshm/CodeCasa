64032771184fe3c47693c6b11f2e96ec3e75c835
NITHYAGANESH M <138594311+nithyaganeshm@users.noreply.github.com>
2023-10-30 09:22:55 +0530

    Add files via upload

---
+++ TODO LIST/assets/script.js
@@ -0,0 +1,38 @@
+const inputBoxElement = document.getElementById('input-box');
+const listContainerElement = document.getElementById('list-container');
+
+function addTask() {
+    if (inputBoxElement.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBoxElement.value;
+        listContainerElement.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBoxElement.value = "";
+    saveData();
+}
+
+listContainerElement.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainerElement.innerHTML);
+}
+function showList() {
+    listContainerElement.innerHTML = localStorage.getItem("data");
+}
+showList();
